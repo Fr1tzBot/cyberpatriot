4bf49772e0a6a780b9dfbfbd07001f228401ba82
Fritz Geib <fritz@standland.org>
2021-11-10 08:31:01 -0500

    better user input

--- users.py
+++ users.py
@@ -1,26 +1,45 @@
 import os
+import json
+from typing import Tuple
+import colorama
 
-print(os.listdir("/home/"))
-print(os.getuid("pram"))
+#Color Functions
+def error(message: str) -> None:
+    print(colorama.Fore.RED + message + colorama.Style.RESET_ALL)
+def warn(message: str) -> None:
+    print(colorama.Fore.YELLOW + message + colorama.Style.RESET_ALL)
+def success(message: str) -> None:
+    print(colorama.Fore.GREEN + message + colorama.Style.RESET_ALL)
 
-running = True
+adminList = ["admin", "a", "administrator", "root", "big man"]
+yesList = ["y", "yes", "ye", "yse", "si", "not no"]
+users = {}
 
-usrslst = []
-permlst = []
-while running:
+prefix = input("enter home dir prefix (/home/): ")
+prefix = "/home/" if prefix == "" else prefix
+
+while True:
     user = input("input user: ")
-    
-    if user == "done":
+    if user == "":
         break
 
-    userperm = input("input (a)dministrator or (s)tandard: ")
+    userperm = input("input (a)dministrator or (s)tandard: ").lower().strip() in adminList
 
-    usrslst.append(user)
-    permlst.append(userperm)
+    if not os.path.exists(prefix + user):
+        warn("WARN: user home directory " + prefix + user + " does not exist")
 
-for usr in os.listdir("/home/"):
-    if usr not in usrslst:
-        os.system(f"sudo userdel -r {usr}")
+    users[user] = {
+        "admin": userperm,
+        "homedir": (prefix + user)
+    }
+for i in users.keys():
+    print(i + ": " + str(users[i]))
 
-    
-    
+if not input("Does the above information look correct? ") in yesList:
+    exit(1)
+
+
+
+#for usr in os.listdir("/home/"):
+#    if usr not in usrslst:
+#        os.system(f"sudo userdel -r {usr}")
